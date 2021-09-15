b1f2de556b27118363f7171f044af9a25e8c5cc3
Rodrigo Sousa <rod_sousa@hotmail.com>
2021-09-15 12:22:22 +0100

    Latex paper summary class 💀

---
+++ papers/cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+latex_papers="./latex"
+
+rm ${latex_papers}/*.aux
+rm ${latex_papers}/*.fdb_latexmk
+rm ${latex_papers}/*.fls
+rm ${latex_papers}/*.log
+rm ${latex_papers}/*.synctex.gz
+rm ${latex_papers}/*.pdf
