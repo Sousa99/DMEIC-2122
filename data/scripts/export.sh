3696780b09b80eb17c65377e8d03609c2a08a3fe
Rodrigo Sousa <rod_sousa@hotmail.com>
2021-11-21 17:10:37 +0000

    Script for exporting data as zip ⚙️

---
+++ data/scripts/export.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NOW=$(date +"%Y.%m.%d %H.%M.%S")
+EXPORT_OUTPUT_FILE="./exports/${NOW} - data.zip"
+
+# Files to compress
+RECORDINGS_DIR="./recordings"
+INFO_PATHS=(
+    "control_info.xlsx"
+    "psychosis_info.xlsx"
+)
+
+cd ..
+zip -r "${EXPORT_OUTPUT_FILE}" $RECORDINGS_DIR ${INFO_PATHS[@]}
