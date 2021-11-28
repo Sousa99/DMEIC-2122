16fdadd3d04f84ea067d47c0be102cba52280293
Rodrigo Sousa <rod_sousa@hotmail.com>
2021-11-28 15:02:26 +0000

    Pre-distribution analysis displays word count 📊

--- analysis/pre-distribution.py
+++ analysis/pre-distribution.py
@@ -18,19 +18,23 @@ warnings.filterwarnings('ignore', category = UserWarning, module = 'openpyxl')
 # =================================== FLAGS PARSING ===================================
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-save",        help="prefix of saved files")
-parser.add_argument("-controls_data",    help="controls data path")
-parser.add_argument("-psychosis_data",   help="psychosis data path")
+parser.add_argument("-save",            help="prefix of saved files")
+parser.add_argument("-controls_data",   help="controls data path")
+parser.add_argument("-psychosis_data",  help="psychosis data path")
 parser.add_argument("-controls_rec",    help="controls recordings path")
 parser.add_argument("-psychosis_rec",   help="psychosis recordings path")
+parser.add_argument("-controls_trans",    help="controls transcriptions path")
+parser.add_argument("-psychosis_trans",   help="psychosis transcriptions path")
 args = parser.parse_args()
 
 requirements = [
     { 'arg': args.save, 'key': 'save', 'help': 'save file name prefix'},
-    { 'arg': args.controls_data, 'key': 'controls_data', 'help': 'path to controls\' data'},
-    { 'arg': args.psychosis_data, 'key': 'psychosis_data', 'help': 'path to psychosis\' data'},
-    { 'arg': args.controls_rec, 'key': 'controls_rec', 'help': 'path to controls\' recordings'},
-    { 'arg': args.psychosis_rec, 'key': 'psychosis_rec', 'help': 'path to psychosis\' recordings'},
+    { 'arg': args.controls_data, 'key': 'controls_data', 'help': 'path to \'controls\' data'},
+    { 'arg': args.psychosis_data, 'key': 'psychosis_data', 'help': 'path to \'psychosis\' data'},
+    { 'arg': args.controls_rec, 'key': 'controls_rec', 'help': 'path to \'controls\' recordings'},
+    { 'arg': args.psychosis_rec, 'key': 'psychosis_rec', 'help': 'path to \'psychosis\' recordings'},
+    { 'arg': args.controls_trans, 'key': 'controls_trans', 'help': 'path to \'controls\' transcriptions'},
+    { 'arg': args.psychosis_trans, 'key': 'psychosis_trans', 'help': 'path to \'psychosis\' transcriptions'},
 ]
 
 if ( any(not req['arg'] for req in requirements) ):
@@ -69,7 +73,7 @@ def retrieveDurationInformation(recordings_path: str, df_info, type: str, tasks:
     subjects_dirs = os.listdir(recordings_path)
     number_of_subjects = len(subjects_dirs)
 
-    printProgressBar(0, number_of_subjects, prefix = 'Processing ' + processing_key + ':', suffix = 'Complete', length = 50)
+    printProgressBar(0, number_of_subjects, prefix = 'Processing \'' + processing_key + '\' duration:', suffix = 'Complete', length = 50)
     for index_value, current_subject in enumerate(subjects_dirs):
 
         current_subject_path = os.path.join(recordings_path, current_subject)
@@ -99,10 +103,49 @@ def retrieveDurationInformation(recordings_path: str, df_info, type: str, tasks:
                 entry[row.lower()] = info
             information.append(entry)
         
-        printProgressBar(index_value + 1, number_of_subjects, prefix = 'Processing ' + processing_key + ':', suffix = 'Complete', length = 50)
+        printProgressBar(index_value + 1, number_of_subjects, prefix = 'Processing \'' + processing_key + '\' duration:', suffix = 'Complete', length = 50)
 
     return information
 
+def retrieveWordLengthInformation(transcriptions_path: str, df_info, type: str, tasks: List[str], processing_key: str):
+    information = []
+    subjects_dirs = os.listdir(transcriptions_path)
+    number_of_subjects = len(subjects_dirs)
+
+    printProgressBar(0, number_of_subjects, prefix = 'Processing \'' + processing_key + '\' word length:', suffix = 'Complete', length = 50)
+    for index_value, current_subject in enumerate(subjects_dirs):
+
+        current_subject_path = os.path.join(transcriptions_path, current_subject)
+        current_subject_id = current_subject.split('_')[1]
+
+        for current_subject_task in os.listdir(current_subject_path):
+
+            current_task_index = int(current_subject_task.replace(current_subject + '_', '')) - 1
+            current_task = tasks[current_task_index]
+            current_task_subject_path = os.path.join(current_subject_path, current_subject_task)
+
+            entry = { 'id': current_subject, 'type': type, 'task': current_task }
+
+            transcription_files = os.listdir(current_task_subject_path)
+            if len(transcription_files) == 0: continue
+
+            total_words = 0
+            for filename in transcription_files:
+
+                transcription_path = os.path.join(current_task_subject_path, filename)
+                file = open(transcription_path, 'r')
+                total_words = total_words + len(file.read().split())
+                file.close()
+
+            entry['words'] = total_words / len(transcription_files)
+            # Append to entry information from xlsx
+            for row, info in zip(df_info.columns, df_info.loc[current_subject_id]):
+                entry[row.lower()] = info
+            information.append(entry)
+        
+        printProgressBar(index_value + 1, number_of_subjects, prefix = 'Processing \'' + processing_key + '\' word length:', suffix = 'Complete', length = 50)
+
+    return information
 
 # =================================== MAIN EXECUTION ===================================
 
@@ -110,32 +153,53 @@ TASKS = [ "Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6", "Task 7" ]
 
 # ======================================================================================
 
-
 # Retrieve Control Data
 worksheet_name = args.controls_data.split('/')[-1].replace('.xlsx', '')
 dataframe_control = pd.read_excel(args.controls_data, sheet_name = worksheet_name, index_col = 0)
-controls_information = retrieveDurationInformation(args.controls_rec, dataframe_control, "Controls", TASKS, 'Controls')
 # Retrieve Psychosis Data
 worksheet_name = args.psychosis_data.split('/')[-1].replace('.xlsx', '')
 dataframe_psychosis = pd.read_excel(args.psychosis_data, sheet_name = worksheet_name, index_col = 0)
-psychosis_information = retrieveDurationInformation(args.psychosis_rec, dataframe_psychosis, "Psychosis", TASKS, 'Psychosis')
+
+# ================================================================== DURATION ==================================================================
+# Retrieve Control Duration
+controls_duration_information = retrieveDurationInformation(args.controls_rec, dataframe_control, "Controls", TASKS, 'Controls')
+# Retrieve Psychosis Duration
+psychosis_duration_information = retrieveDurationInformation(args.psychosis_rec, dataframe_psychosis, "Psychosis", TASKS, 'Psychosis')
 
 # Merge information into dataframe
-information = controls_information + psychosis_information
-task_df = pd.DataFrame(information)
-task_df.set_index('id') 
+duration_information = controls_duration_information + psychosis_duration_information
+task_duration_df = pd.DataFrame(duration_information)
+task_duration_df.set_index('id') 
 #print(task_df)
 
 sns.set_theme(palette="deep")
 
 plt.clf()
-sns.displot(data=task_df, x="duration", col="task", hue='type', multiple='dodge', kde=True, common_bins=False)
+sns.displot(data=task_duration_df, x="duration", col="task", hue='type', multiple='dodge', kde=True, facet_kws=dict(sharex=False, sharey=False), common_bins=True)
 plt.savefig(args.save + ' - task duration by type.png')
 
 plt.clf()
-sns.displot(data=task_df, x="duration", row="gender", col="task", hue='type', multiple='dodge', kde=True, common_bins=False)
+sns.displot(data=task_duration_df, x="duration", row="gender", col="task", hue='type', multiple='dodge', kde=True, facet_kws=dict(sharex=False, sharey=False), common_bins=True)
 plt.savefig(args.save + ' - task duration by gender.png')
 
 plt.clf()
-sns.displot(data=task_df, x="duration", row="schooling", col="task", hue='type', multiple='dodge', kde=True, common_bins=False)
+sns.displot(data=task_duration_df, x="duration", row="schooling", col="task", hue='type', multiple='dodge', kde=True, facet_kws=dict(sharex=False, sharey=False), common_bins=True)
 plt.savefig(args.save + ' - task duration by schooling.png')
+
+# ================================================================== WORD COUNT ==================================================================
+# Retrieve Control Duration
+controls_word_count_information = retrieveWordLengthInformation(args.controls_trans, dataframe_control, "Controls", TASKS, 'Controls')
+# Retrieve Psychosis Duration
+psychosis_word_count_information = retrieveWordLengthInformation(args.psychosis_trans, dataframe_psychosis, "Psychosis", TASKS, 'Psychosis')
+
+# Merge information into dataframe
+word_count_information = controls_word_count_information + psychosis_word_count_information
+task_word_count_df = pd.DataFrame(word_count_information)
+task_word_count_df.set_index('id') 
+#print(task_df)
+
+sns.set_theme(palette="deep")
+
+plt.clf()
+sns.displot(data=task_word_count_df, x="words", col="task", hue='type', multiple='dodge', kde=True, facet_kws=dict(sharex=False, sharey=False), common_bins=True)
+plt.savefig(args.save + ' - word count by type.png')
--- analysis/run_analysis.sh
+++ analysis/run_analysis.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 CONTROL="../data/control_info.xlsx"
-CONTROL_RECORDS="../data/recordings/controls/"
 PSYCHOSIS="../data/psychosis_info.xlsx"
+CONTROL_RECORDS="../data/recordings/controls/"
 PSYCHOSIS_RECORDS="../data/recordings/psychosis/"
+CONTROL_TRANSCRIPTIONS="../data/recordings_transcribed_results/controls_transcribed_results/"
+PSYCHOSIS_TRANSCRIPTIONS="../data/recordings_transcribed_results/psychosis_transcribed_results/"
 
 NOW=$(date +"%Y.%m.%d %H.%M.%S")
 DIR="./records/${NOW}/"
@@ -18,5 +20,5 @@ echo
 # run pre-distribution analysis
 echo "📊 Pre Distribution Analysis"
 OUTPUT_FILE="$DIR${NOW} - pre-distribution"
-python3 ./pre-distribution.py -controls_data "${CONTROL}" -psychosis_data "${PSYCHOSIS}" -controls_rec "${CONTROL_RECORDS}" -psychosis_rec "${PSYCHOSIS_RECORDS}" -save "${OUTPUT_FILE}"
+python3 ./pre-distribution.py -controls_data "${CONTROL}" -psychosis_data "${PSYCHOSIS}" -controls_rec "${CONTROL_RECORDS}" -psychosis_rec "${PSYCHOSIS_RECORDS}" -controls_trans "${CONTROL_TRANSCRIPTIONS}" -psychosis_trans "${PSYCHOSIS_TRANSCRIPTIONS}" -save "${OUTPUT_FILE}"
 echo
