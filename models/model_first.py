a1450d52418b110932ebb1f04444720f5de4d4ad
Rodrigo Sousa <rod_sousa@hotmail.com>
2022-02-13 20:32:03 +0000

    First model runs various variation in single execution 📦

--- models/model_first.py
+++ models/model_first.py
@@ -51,7 +51,7 @@ if ( any(not req['arg'] for req in requirements) ):
 
 # =================================== DEBUG CONSTANTS ===================================
 
-DATASET_SAMPLE = 0.1
+DATASET_SAMPLE = 1.0
 
 # =================================== EXECUTION CONSTANTS ===================================
 
@@ -99,9 +99,6 @@ speech_features_df = module_speech_features.speech_analysis(subject_paths, PREFE
 all_features_df = pd.merge(sound_features_df, speech_features_df, left_index=True, right_index=True, how='outer', suffixes=('', '_duplicate'))
 all_features_df = all_features_df.drop(all_features_df.filter(regex='_duplicate$').columns.tolist(), axis=1)
 
-# ===================================== CHOOSE TASKS =====================================
-ACTIVE_TASKS = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5', 'Task 6', 'Task 7']
-
 # ===================================== DROP FEATURES =====================================
 general_drop_columns = ['Subject', 'Task']
 sound_drop_columns = ['Audio Path', 'Audio File', 'Audio File Path', 'Trans Path']
@@ -114,40 +111,60 @@ speech_features = [column for column in speech_features_df.columns.values if col
 all_features = [column for column in all_features_df.columns.values if column not in all_features_drop_columns + general_drop_columns]
 
 # ===================================== DATAFRAME TO USE =====================================
-dataframe = all_features_df
-dataframe_drop_columns = all_features_drop_columns
-dataframe_features = all_features
+print()
+variations_to_test = [
+    { 'key': 'SVM - Speech Features - All Tasks', 'dataframe': speech_features_df.copy(deep=True), 
+        'drop_columns': speech_drop_columns, 'feature_columns': speech_features,
+        'tasks': ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5', 'Task 6', 'Task 7'],
+        'classifier': module_classifier.SupportVectorMachine,
+        },
+    { 'key': 'SVM - All Features - All Tasks', 'dataframe': all_features_df.copy(deep=True), 
+        'drop_columns': all_features_drop_columns, 'feature_columns': all_features,
+        'tasks': ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5', 'Task 6', 'Task 7'],
+        'classifier': module_classifier.SupportVectorMachine,
+        },
+]
 
-# Drop unwanted columns and pivot on tasks
-dataframe = dataframe[dataframe['Task'].isin(ACTIVE_TASKS)]
-dataframe = dataframe.drop(dataframe_drop_columns, axis=1)
-dataframe_pivot = module_aux.pivot_on_column(dataframe, ['Subject'], 'Task', dataframe_features, 'on')
+print("🚀 Running solution variations ...")
+for variation in variations_to_test:
 
-# Export dataframe to use
-module_exporter.export_csv(dataframe_pivot, 'dataset')
+    print("🚀 Running '{0}'".format(variation['key']))
+    module_exporter.change_current_model_directory(variation['key'])
 
-# Sepparate features and target class
-dataframe_X = dataframe_pivot
-dataframe_Y = dataframe_pivot.reset_index()['Subject'].apply(lambda subject: subject_info.loc[subject]['Target'])
-dataframe_Y.index = dataframe_pivot.index
+    dataframe = variation['dataframe']
+    dataframe_drop_columns = variation['drop_columns']
+    dataframe_features = variation['feature_columns']
 
-# ===== FIXME: MOVE TO ANOTHER MODULE =====
-imp = SimpleImputer(strategy='mean', missing_values=np.nan, copy=True)
-dataframe_X = pd.DataFrame(imp.fit_transform(dataframe_X), columns=dataframe_X.columns.values)
-# ===== FIXME: MOVE TO ANOTHER MODULE =====
+    # Drop unwanted columns and pivot on tasks
+    dataframe = dataframe[dataframe['Task'].isin(variation['tasks'])]
+    dataframe = dataframe.drop(dataframe_drop_columns, axis=1)
+    dataframe_pivot = module_aux.pivot_on_column(dataframe, ['Subject'], 'Task', dataframe_features, 'on')
 
-speech_splits = list(module_classifier.leave_one_out(dataframe_X))
-scorer = module_scorer.Scorer(['Psychosis', 'Control'])
-with alive_bar(len(speech_splits)) as bar:
-    for (train_index, test_index) in speech_splits:
-        X_train, X_test = dataframe_X.iloc[train_index], dataframe_X.iloc[test_index]
-        y_train, y_test = dataframe_Y.iloc[train_index], dataframe_Y.iloc[test_index]
+    # Export dataframe to use
+    module_exporter.export_csv(dataframe_pivot, 'dataset')
 
-        classifier = module_classifier.SupportVectorMachine()
-        y_prd = classifier.make_prediction(X_train, y_train, X_test)
-        scorer.add_points(y_test, y_prd)
+    # Sepparate features and target class
+    dataframe_X = dataframe_pivot
+    dataframe_Y = dataframe_pivot.reset_index()['Subject'].apply(lambda subject: subject_info.loc[subject]['Target'])
+    dataframe_Y.index = dataframe_pivot.index
 
-        # Update Progress Bar
-        bar()
+    # ===== FIXME: MOVE TO ANOTHER MODULE =====
+    imp = SimpleImputer(strategy='mean', missing_values=np.nan, copy=True)
+    dataframe_X = pd.DataFrame(imp.fit_transform(dataframe_X), columns=dataframe_X.columns.values)
+    # ===== FIXME: MOVE TO ANOTHER MODULE =====
 
-scorer.export_results('results')
+    speech_splits = list(module_classifier.leave_one_out(dataframe_X))
+    scorer = module_scorer.Scorer(['Psychosis', 'Control'])
+    with alive_bar(len(speech_splits), title='👉 Running model in ' + variation['key']) as bar:
+        for (train_index, test_index) in speech_splits:
+            X_train, X_test = dataframe_X.iloc[train_index], dataframe_X.iloc[test_index]
+            y_train, y_test = dataframe_Y.iloc[train_index], dataframe_Y.iloc[test_index]
+
+            classifier = variation['classifier']()
+            y_prd = classifier.make_prediction(X_train, y_train, X_test)
+            scorer.add_points(y_test, y_prd)
+
+            # Update Progress Bar
+            bar()
+
+    scorer.export_results('results')
