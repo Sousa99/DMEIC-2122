3db41332ec749e084f01e3c79f655b9a308c2aa2
Sousa99 <rod_sousa@hotmail.com>
2022-04-18 22:16:55 +0100

    Developed module for NLP and Gensim ➕ - Updated requirements

--- models/modules_aux/module_nlp.py
+++ models/modules_aux/module_nlp.py
@@ -1,9 +1,28 @@
+import abc
+import stanza
 
-# =================================== PRIVATE METHODS ===================================
+from typing import List
 
 # =================================== PRIVATE CLASS DEFINITIONS ===================================
 
+class Lemmatizer(abc.ABC):
+
+    @abc.abstractmethod
+    def __init__(self) -> None: super().__init__()
+    @abc.abstractmethod
+    def get_name(self) -> str: exit(f"🚨 Method 'get_name' not defined for '{self.__class__.__name__}'")
+    @abc.abstractmethod
+    def process_words(self, words: List[str]) -> List[str]: exit(f"🚨 Method 'process_words' not defined for '{self.__class__.__name__}'")
+
 # =================================== PUBLIC CLASS DEFINITIONS ===================================
 
-# =================================== PUBLIC METHODS ===================================
+class LemmatizerStanza(Lemmatizer):
 
+    pipeline = stanza.Pipeline('pt', verbose=False)
+
+    def __init__(self) -> None: super().__init__()
+    def get_name(self) -> str: return "Stanza"
+    def process_words(self, words: List[str]) -> List[str]:
+        words_as_string : str = ' '.join(words)
+        processed = self.pipeline(words_as_string)
+        return [ word.lemma for sentence in processed.sentences for word in sentence.words ]
---
+++ models/modules_corpora/module_gensim.py
@@ -0,0 +1,44 @@
+import os
+import abc
+from typing import List, Optional
+import gensim
+
+
+# =================================== CONSTANTS DEFINITIONS ===================================
+
+PATH_MODELS_SUPPORT = '../models_support/exports/'
+
+FILENAME_DICTIONARY = 'corpora_dictionary.bin'
+FILENAME_LSA        = 'lsa_best_model.bin'
+
+# =================================== PRIVATE CLASS DEFINITIONS ===================================
+
+class ModelCorpora(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def get_word_embedding(self, word: str) -> Optional[List[float]]:
+        pass
+
+# =================================== PUBLIC CLASS DEFINITIONS ===================================
+
+class ModelLSA(ModelCorpora):
+
+    def __init__(self) -> None:
+        filepath_dictionary = os.path.join(PATH_MODELS_SUPPORT, FILENAME_DICTIONARY)
+        if not os.path.exists(filepath_dictionary): exit(f'🚨 The file \'{filepath_dictionary}\' does not exist')
+        filepath_model = os.path.join(PATH_MODELS_SUPPORT, FILENAME_LSA)
+        if not os.path.exists(filepath_model): exit(f'🚨 The file \'{filepath_model}\' does not exist')
+
+        self.dictionary : gensim.corpora.Dictionary = gensim.corpora.Dictionary.load(filepath_dictionary)
+        self.model : gensim.models.LsiModel = gensim.models.LsiModel.load(filepath_model)
+
+    def get_word_embedding(self, word: str) -> Optional[List[float]]:
+
+        if word not in self.dictionary.token2id: return None
+        word_id = self.dictionary.token2id[word]
+        embedding = self.model.projection.u[word_id]
+
+        return embedding
