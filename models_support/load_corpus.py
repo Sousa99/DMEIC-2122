95ae03ea6762dc4a4f2f65d450f639684dd81f9c
Sousa99 <rod_sousa@hotmail.com>
2022-04-18 10:26:50 +0100

    Not loaded corpus abstraction can debug progress ➕

--- models_support/load_corpus.py
+++ models_support/load_corpus.py
@@ -1,10 +1,11 @@
 import os
 import abc
+import tqdm
 import gensim
 import pickle
 
 from random import random
-from typing import List, Optional
+from typing import Iterable, List, Optional
 
 # ============================================================ MAIN CLASS DEFINITION ============================================================
 
@@ -22,18 +23,23 @@ class CorpusNotLoaded(abc.ABC):
 
 class LemmatizedCorpusNotLoaded(CorpusNotLoaded):
 
-    def __init__(self, path_to_corpora_files: str, number_of_documents_to_use: Optional[int] = None) -> None:
+    def __init__(self, path_to_corpora_files: str, number_of_documents_to_use: Optional[int] = None, debug_iteration: bool = False) -> None:
         super().__init__()
         
         self.path_to_corpora : str = path_to_corpora_files
         self.filenames : List[str] = os.listdir(path_to_corpora_files)
+        self.debug_iteration : bool = debug_iteration
 
         if number_of_documents_to_use is not None:
             self.filenames = random.sample(self.filenames, number_of_documents_to_use)
 
     def __iter__(self):
 
-        for filename in self.filenames:
+        filename_iterator : Iterable[str] = self.filenames
+        if self.debug_iteration:
+            filename_iterator = tqdm.tqdm(filename_iterator, desc="🚀 Reading Documents for Lemmatized Corpus", leave=True)
+
+        for filename in filename_iterator:
 
             file_path = os.path.join(self.path_to_corpora, filename)
             if not os.path.isfile(file_path): continue
@@ -46,19 +52,24 @@ class LemmatizedCorpusNotLoaded(CorpusNotLoaded):
 
 class BOWCorpusNotLoaded(CorpusNotLoaded):
 
-    def __init__(self, path_to_corpora_files: str, dictionary: gensim.corpora.Dictionary, number_of_documents_to_use: Optional[int] = None) -> None:
+    def __init__(self, path_to_corpora_files: str, dictionary: gensim.corpora.Dictionary, number_of_documents_to_use: Optional[int] = None, debug_iteration: bool = False) -> None:
         super().__init__()
 
         self.path_to_corpora : str = path_to_corpora_files
         self.dictionary : gensim.corpora.Dictionary = dictionary
         self.filenames : List[str] = os.listdir(path_to_corpora_files)
+        self.debug_iteration : bool = debug_iteration
 
         if number_of_documents_to_use is not None:
             self.filenames = random.sample(self.filenames, number_of_documents_to_use)
 
     def __iter__(self):
 
-        for filename in self.filenames:
+        filename_iterator : Iterable[str] = self.filenames
+        if self.debug_iteration:
+            filename_iterator = tqdm.tqdm(filename_iterator, desc="🚀 Reading Documents for BOW Corpus", leave=True)
+
+        for filename in filename_iterator:
 
             file_path = os.path.join(self.path_to_corpora, filename)
             if not os.path.isfile(file_path): continue
