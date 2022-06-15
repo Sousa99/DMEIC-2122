46f3b2cc0083ff7d300ce17b6ea1b6acde4a90b7
Sousa99 <rod_sousa@hotmail.com>
2022-06-15 20:44:02 +0100

    Developed parallelization scripts for each model ➕

---
+++ models/run_model_first_parallelized.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+CURRENT_DIR=`pwd`
+NOW=$(date +"%Y.%m.%d %H.%M.%S")
+
+typeset -i WAIT_SECONDS=$("20")
+typeset -i MAX_JOBS_PER_MACHINE=$("4")
+
+CONTROL_INFO="../data/control_info.xlsx"
+PSYCHOSIS_INFO="../data/psychosis_info.xlsx"
+BIPOLAR_INFO="../data/bipolar_info.xlsx"
+
+CONTROL_AUDIOS="../data/recordings_converted/controls/"
+PSYCHOSIS_AUDIOS="../data/recordings_converted/psychosis/"
+BIPOLAR_AUDIOS="../data/recordings_converted/bipolars/"
+
+CONTROL_TRANSCRIPTIONS="../data/fixed_transcriptions/controls/"
+CONTROL_TRANSCRIPTIONS="../data/recordings_transcribed_results/controls/"
+PSYCHOSIS_TRANSCRIPTIONS="../data/fixed_transcriptions/psychosis/"
+PSYCHOSIS_TRANSCRIPTIONS="../data/recordings_transcribed_results/psychosis/"
+BIPOLAR_TRANSCRIPTIONS="../data/fixed_transcriptions/bipolars/"
+BIPOLAR_TRANSCRIPTIONS="../data/recordings_transcribed_results/bipolars/"
+
+VARIATION_KEY="simple"
+
+python3 model_first.py                                                                                                                      \
+    -info_controls=${CONTROL_INFO}              -info_psychosis=${PSYCHOSIS_INFO}               -info_bipolars=${BIPOLAR_INFO}              \
+    -audio_controls=${CONTROL_AUDIOS}           -audio_psychosis=${PSYCHOSIS_AUDIOS}            -audio_bipolars=${BIPOLAR_AUDIOS}           \
+    -trans_controls=${CONTROL_TRANSCRIPTIONS}   -trans_psychosis=${PSYCHOSIS_TRANSCRIPTIONS}    -trans_bipolars=${BIPOLAR_TRANSCRIPTIONS}   \
+    -parallelization_key="FEATURE_EXTRACTION"                                                                                               \
+    -timestamp="${NOW}"                                                                                                                     \
+    -variations_key=${VARIATION_KEY}
+
+python3 ./parallelization.py -timestamp="${NOW}" -execution="init" -wait_seconds=${WAIT_SECONDS} -max_jobs_per_machine=${MAX_JOBS_PER_MACHINE}
+ret=$?
+if [ $ret -ne 0 ]; then exit; fi
+
+echo
+echo "🚀 Developing solution variations ..."
+typeset -i number_of_variations=$(cat "./tmp/${NOW}/tmp_number_variations.txt")
+for parallel_index in $(seq 0 $(expr $number_of_variations - 1)); do
+    process_id=$(printf "first_variation_%05d" $parallel_index)
+    script_file="${TEMP_CONDOR_SCRIPTS_DIRECTORY}${process_id}.sh"
+
+    echo "#!/bin/bash" > "${script_file}"
+    echo "cd ${CURRENT_DIR}"                                                                                                                                >> "${script_file}"
+    echo "source ./venv/bin/activate"                                                                                                                       >> "${script_file}"
+    echo "python3 model_first.py		                                                                                                                \\" >> "${script_file}"
+    echo "      -info_controls=${CONTROL_INFO}              -info_psychosis=${PSYCHOSIS_INFO}               -info_bipolars=${BIPOLAR_INFO}              \\" >> "${script_file}"
+    echo "      -audio_controls=${CONTROL_AUDIOS}           -audio_psychosis=${PSYCHOSIS_AUDIOS}            -audio_bipolars=${BIPOLAR_AUDIOS}           \\" >> "${script_file}"
+    echo "      -trans_controls=${CONTROL_TRANSCRIPTIONS}   -trans_psychosis=${PSYCHOSIS_TRANSCRIPTIONS}    -trans_bipolars=${BIPOLAR_TRANSCRIPTIONS}   \\" >> "${script_file}"
+    echo "      -parallelization_key=\"RUN_MODELS\"         -parallelization_index=${parallel_index}                                                    \\" >> "${script_file}"
+    echo "      -timestamp=\"${NOW}\"                                                                                                                   \\" >> "${script_file}"
+    echo "      -variations_key=\"${VARIATION_KEY}\""                                                                                                       >> "${script_file}"
+
+    chmod a+x "${script_file}"
+    python3 ./parallelization.py -timestamp="${NOW}" -execution="add" -execution_id="${process_id}" -execution_file="${script_file}"
+	ret=$?
+	if [ $ret -ne 0 ]; then exit; fi
+done
+
+python3 ./parallelization.py -timestamp="${NOW}" -execution="run"
+ret=$?
+if [ $ret -ne 0 ]; then exit; fi
+
+python3 model_first.py                                                                                                                      \
+    -info_controls=${CONTROL_INFO}              -info_psychosis=${PSYCHOSIS_INFO}               -info_bipolars=${BIPOLAR_INFO}              \
+    -audio_controls=${CONTROL_AUDIOS}           -audio_psychosis=${PSYCHOSIS_AUDIOS}            -audio_bipolars=${BIPOLAR_AUDIOS}           \
+    -trans_controls=${CONTROL_TRANSCRIPTIONS}   -trans_psychosis=${PSYCHOSIS_TRANSCRIPTIONS}    -trans_bipolars=${BIPOLAR_TRANSCRIPTIONS}   \
+    -parallelization_key="RUN_FINAL"                                                                                                        \
+    -timestamp="${NOW}"                                                                                                                     \
+    -variations_key=${VARIATION_KEY}
