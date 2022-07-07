6188aaba6acedd53f2fcd4a31bc36f6cd8398ad4
Sousa99 <rod_sousa@hotmail.com>
2022-07-07 20:35:14 +0100

    TrustPilot more concrete and specific state 🐛

--- models_support/web_scraping/scraper_trustpilot.py
+++ models_support/web_scraping/scraper_trustpilot.py
@@ -49,12 +49,13 @@ class WebScraperTrustPilot(scraper.WebScraper[ScrapedInfoTrustPilot]):
     REVIEWS_LINK : str = 'https://pt.trustpilot.com/categories/money_insurance'
 
     def __init__(self) -> None:
-        super().__init__('TrustPilot', '0.1', { 'current_page': 0, 'reviews_page': 0 })
+        state = { 'current_page': 1, 'number_companies_parsed': 0 ,
+            'reviews_page': 1, 'number_reviews_parsed': 0 }
+        super().__init__('TrustPilot', '0.2', state)
 
     def get_pages_to_scrape(self, driver: driver.Driver) -> Generator[str, None, None]:
 
         while True:
-            self.state['current_page'] = self.state['current_page'] + 1
 
             # ============================ In fact get page with important information ============================
             driver.driver_get(f"{self.REVIEWS_LINK}?page={self.state['current_page']}")
@@ -66,18 +67,25 @@ class WebScraperTrustPilot(scraper.WebScraper[ScrapedInfoTrustPilot]):
             if len(listings) == 2: listing = listings[1]
             # Companies: Get various companies
             companies_listed = listing.find_all('div', class_="card_card__2F_07")
-            if len(companies_listed) == 0: return
+            if len(companies_listed) == 0: break
             # Reader's Reviews: Iterate and Get Link
+            for _ in range(self.state['number_companies_parsed'] - 1): companies_listed.pop(0)
             for company_card in companies_listed:
+                # Update State
+                self.state['number_companies_parsed'] = self.state['number_companies_parsed'] + 1
+
                 review_item_link = company_card.find('a')
                 yield f"{self.BASE_LINK}{review_item_link['href']}"
-
-            exit(0)
+                # Update State
+                self.state['reviews_page'] = 1
+            
+            # Update State
+            self.state['current_page'] = self.state['current_page'] + 1
+            self.state['number_companies_parsed'] = 0
 
     def scrape_page(self, link: str, driver: driver.Driver) -> Generator[ScrapedInfoTrustPilot, None, None]:
         
         while True:
-            self.state['reviews_page'] = self.state['reviews_page'] + 1
 
             # ============================ In fact get page with reviews information ============================
             link_to_check = f"{link}?page={self.state['reviews_page']}"
@@ -92,7 +100,10 @@ class WebScraperTrustPilot(scraper.WebScraper[ScrapedInfoTrustPilot]):
                 return
             
             review_cards = link_soup.find_all('article', attrs={"data-service-review-card-paper": "true"})
+            for _ in range(self.state['number_reviews_parsed']): review_cards.pop(0)
             for review_card in review_cards:
+                # Update State
+                self.state['number_reviews_parsed'] = self.state['number_reviews_parsed'] + 1
 
                 review_from = review_card.find('div', attrs={"data-consumer-name-typography": "true"}).get_text()
 
@@ -100,9 +111,9 @@ class WebScraperTrustPilot(scraper.WebScraper[ScrapedInfoTrustPilot]):
                 if review_score_elem is None: continue
                 review_score = float(review_score_elem["data-service-review-rating"])
 
-                review_text_elem = review_card.find('p', attrs={"data-service-review-text-typography": True})
+                review_text_elem = review_card.find('p', attrs={"data-service-review-text-typography": "true"})
                 if review_text_elem is None: continue
-                review_text = review_score_elem.get_text()
+                review_text = review_text_elem.get_text()
 
                 review_date_elem = review_card.find('time', attrs={"data-service-review-date-time-ago": "true"})
                 if review_date_elem is None: continue
@@ -111,6 +122,10 @@ class WebScraperTrustPilot(scraper.WebScraper[ScrapedInfoTrustPilot]):
 
                 yield ScrapedInfoTrustPilot(company_name, TRUST_PILOT_SCORE_FLOOR, TRUST_PILOT_SCORE_CEIL,
                     review_from, review_text, review_score, review_date)
+            
+            # Update State
+            self.state['reviews_page'] = self.state['reviews_page'] + 1
+            self.state['number_reviews_parsed'] = 0
 
     def callback_accessible(self, page_source: str) -> bool:
         if "Access Denied" in page_source: return False
@@ -122,6 +137,6 @@ class WebScraperTrustPilot(scraper.WebScraper[ScrapedInfoTrustPilot]):
 # ============================================================ MAIN FUNCTIONALITY ============================================================
 
 scraper_to_use : WebScraperTrustPilot = WebScraperTrustPilot()
-request_driver : driver.Driver = driver.Driver(rotate_proxies=True, rotate_proxies_rand=True, rotate_user_agents=True, max_requests=200, max_attempts_driver=20)
+request_driver : driver.Driver = driver.Driver(rotate_proxies=True, rotate_proxies_rand=False, rotate_user_agents=True, max_requests=200, max_attempts_driver=20)
 request_driver.set_callback_accessible(scraper_to_use.callback_accessible)
 scraper_valence.run_scraper(scraper_to_use, request_driver)
