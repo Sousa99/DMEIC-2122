be7a73d4c0eef7dbcefa46805ba7af154d10689f
Sousa99 <rod_sousa@hotmail.com>
2022-07-15 15:46:39 +0100

    TrustPilot webscraper multiple categories ➕

--- models_support/web_scraping/scraper_trustpilot.py
+++ models_support/web_scraping/scraper_trustpilot.py
@@ -1,5 +1,7 @@
-from typing import Any, Dict, Generator
-from bs4    import BeautifulSoup
+from selenium   import webdriver
+from typing     import Any, Dict, Generator, List
+from bs4        import BeautifulSoup
+
 
 import driver
 import scraper
@@ -46,42 +48,59 @@ class ScrapedInfoTrustPilot(scraper.ScrapedInfoValence):
 class WebScraperTrustPilot(scraper.WebScraper[ScrapedInfoTrustPilot]):
 
     BASE_LINK : str = 'https://pt.trustpilot.com'
-    REVIEWS_LINK : str = 'https://pt.trustpilot.com/categories/money_insurance?status=all&timeperiod=0&'
+    REVIEWS_LINKS : List[str] = [
+        'https://pt.trustpilot.com/categories/money_insurance',
+        'https://pt.trustpilot.com/categories/vehicles_transportation',
+        'https://pt.trustpilot.com/categories/jewelry_store',
+        'https://pt.trustpilot.com/categories/clothing_store',
+        'https://pt.trustpilot.com/categories/electronics_technology',
+        'https://pt.trustpilot.com/categories/fitness_and_nutrition_service'
+    ]
 
     def __init__(self) -> None:
-        state = { 'current_page': 1, 'number_companies_parsed': 0 ,
+        state = { 'current_category': 0,
+            'current_page': 1, 'number_companies_parsed': 0 ,
             'reviews_page': 1, 'number_reviews_parsed': 0 }
         super().__init__('TrustPilot', '0.2', state)
 
     def get_pages_to_scrape(self, driver: driver.Driver) -> Generator[str, None, None]:
 
-        while True:
+        review_links_to_use = self.REVIEWS_LINKS[self.state['current_category']:]
+        for review_link in review_links_to_use:
+            review_link_total = f"{review_link}?numberofreviews=0&status=all&timeperiod=0&"
 
-            # ============================ In fact get page with important information ============================
-            driver.driver_get(f"{self.REVIEWS_LINK}?page={self.state['current_page']}")
-            link_soup = BeautifulSoup(driver.driver_page_source(), 'html.parser')
+            while True:
 
-            # Listings: Get the various listings and select one
-            listings = link_soup.find_all('div', class_="styles_businessUnitCardsContainer__1ggaO")
-            listing = listings[0]
-            if len(listings) == 2: listing = listings[1]
-            # Companies: Get various companies
-            companies_listed = listing.find_all('div', class_="card_card__2F_07")
-            if len(companies_listed) == 0: break
-            # Reader's Reviews: Iterate and Get Link
-            for _ in range(self.state['number_companies_parsed'] - 1): companies_listed.pop(0)
-            for company_card in companies_listed:
+                # ============================ In fact get page with important information ============================
+                driver.driver_get(f"{review_link_total}page={self.state['current_page']}")
+                link_soup = BeautifulSoup(driver.driver_page_source(), 'html.parser')
+
+                # Listings: Get the various listings and select one
+                listings = link_soup.find_all('div', class_="styles_businessUnitCardsContainer__1ggaO")
+                if (len(listings)) == 0: break
+                listing = listings[0]
+                if len(listings) == 2: listing = listings[1]
+                # Companies: Get various companies
+                companies_listed = listing.find_all('div', class_="card_card__2F_07")
+                if len(companies_listed) == 0: break
+                # Reader's Reviews: Iterate and Get Link
+                for _ in range(self.state['number_companies_parsed'] - 1): companies_listed.pop(0)
+                for company_card in companies_listed:
+                    # Update State
+                    self.state['number_companies_parsed'] = self.state['number_companies_parsed'] + 1
+
+                    review_item_link = company_card.find('a')
+                    yield f"{self.BASE_LINK}{review_item_link['href']}"
+                    # Update State
+                    self.state['reviews_page'] = 1
+                
                 # Update State
-                self.state['number_companies_parsed'] = self.state['number_companies_parsed'] + 1
+                self.state['current_page'] = self.state['current_page'] + 1
+                self.state['number_companies_parsed'] = 0
 
-                review_item_link = company_card.find('a')
-                yield f"{self.BASE_LINK}{review_item_link['href']}"
-                # Update State
-                self.state['reviews_page'] = 1
-            
             # Update State
-            self.state['current_page'] = self.state['current_page'] + 1
-            self.state['number_companies_parsed'] = 0
+            self.state['current_category'] = self.state['current_category'] + 1
+            self.state['current_page'] = 1
 
     def scrape_page(self, link: str, driver: driver.Driver) -> Generator[ScrapedInfoTrustPilot, None, None]:
         
@@ -91,6 +110,12 @@ class WebScraperTrustPilot(scraper.WebScraper[ScrapedInfoTrustPilot]):
             link_to_check = f"{link}?page={self.state['reviews_page']}"
             driver.driver_get(link_to_check)
             link_soup = BeautifulSoup(driver.driver_page_source(), 'html.parser')
+
+            def wait_until_loaded(driver: webdriver.Chrome) -> bool:
+                link_soup = BeautifulSoup(driver.page_source, 'html.parser')
+                return link_soup.find('span', class_="title_displayName__TtDDM") != None
+            try: driver.driver_wait_until(wait_until_loaded)
+            except: pass
 
             company_name = link_soup.find('span', class_="title_displayName__TtDDM").get_text().strip()
 
@@ -132,11 +157,13 @@ class WebScraperTrustPilot(scraper.WebScraper[ScrapedInfoTrustPilot]):
         elif "This site can’t be reached" in page_source: return False
         elif "Your connection is not private" in page_source: return False
         elif "No internet" in page_source: return False
+        elif "This page isn’t working" in page_source: return False
+        elif "We have received an unusually large amount of requests from your IP so you have been rate limited" in page_source: return False
         return True
 
 # ============================================================ MAIN FUNCTIONALITY ============================================================
 
 scraper_to_use : WebScraperTrustPilot = WebScraperTrustPilot()
-request_driver : driver.Driver = driver.Driver(rotate_proxies=True, rotate_proxies_rand=False, rotate_user_agents=True, max_requests=200, max_attempts_driver=20)
+request_driver : driver.Driver = driver.Driver(rotate_proxies=True, rotate_proxies_rand=True, rotate_user_agents=True, max_requests=200, max_attempts_driver=20)
 request_driver.set_callback_accessible(scraper_to_use.callback_accessible)
 scraper_valence.run_scraper(scraper_to_use, request_driver)
