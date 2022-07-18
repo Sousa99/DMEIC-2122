00cee6f1eee84f01e6453c3fbab4b10f03c4da9b
Sousa99 <rod_sousa@hotmail.com>
2022-07-19 00:10:34 +0100

    Development of roberta model with valence information ➕ - Missing removal of non alphanumeric charcathers from text - Missing testing (no connection)

--- models_support/web_scraping/extract_roberta_valence.py
+++ models_support/web_scraping/extract_roberta_valence.py
@@ -1,11 +1,14 @@
 import os
+import sys
 import json
+import torch
 import argparse
+import transformers
 
 import pandas   as pd
 
 from tqdm       import tqdm
-from typing     import List
+from typing     import Dict, List, Optional, Tuple
 
 # =============================??????????????????????????????????====== PACKAGES PARAMETERS ============??????????????????????????????????=======================
 
@@ -23,6 +26,15 @@ LOAD_METADATA_EXTRACT   : List[str] = ['scraper']
 LOAD_DROP_COLUMNS       : List[str] = ['metadata']
 
 PREPROCESS_TEXT         : bool      = True
+
+SEQUENCE_MAX_LENGTH     : int       = 512
+SEQUENCE_TRUNCATION     : bool      = True
+SEQUENCE_PADDING        : str       = 'max_length'
+
+EXPORTS_DIRECTORY       : str       = '../exports/web_scraping'
+FINAL_MODEL_SAVE        : str       = f'{EXPORTS_DIRECTORY}/valence_roberta'
+ROBERTA_OUTPUT_DIR      : str       = f'{EXPORTS_DIRECTORY}/valence_roberta_outputs/'
+ROBERTA_LOGGING_DIR     : str       = f'{EXPORTS_DIRECTORY}/valence_roberta_logging/'
 
 # ======================================================================== GET ARGUMENTS ========================================================================
 
@@ -43,7 +55,72 @@ def preprocess_datafram(dataframe: pd.DataFrame) -> pd.DataFrame:
 
 def preprocess_text(text: str) -> str:
     if PREPROCESS_TEXT: text = text.lower()
+    # TODO: Remove non alphanumeric characthers
     return text
+
+def get_training_args(output_dir: str, logging_dir: str) -> transformers.TrainingArguments:
+    training_args = transformers.TrainingArguments(
+        output_dir=output_dir,
+        num_train_epochs=3,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        warmup_steps=500,
+        weight_decay=0.01,
+        logging_strategy='no',
+        logging_dir=logging_dir,
+        logging_steps=100,
+        optim="adamw_torch",
+        disable_tqdm=True
+    )
+
+    return training_args
+
+def get_xlm_roberta_large() -> Tuple[transformers.AutoTokenizer, transformers.AutoModelForSequenceClassification]:
+    tokenizer   : transformers.AutoTokenizer                        = transformers.AutoTokenizer.from_pretrained('xlm-roberta-large')
+    model       : transformers.AutoModelForSequenceClassification   = transformers.AutoModelForSequenceClassification.from_pretrained("xlm-roberta-large", num_labels=2)
+
+    return (tokenizer, model)
+
+# ========================================================================= AUX CLASSES =========================================================================
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: val[idx].clone().detach() for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+class TransformerModel():
+
+    def __init__(self, tokenizer: transformers.PreTrainedTokenizer, model: transformers.PreTrainedModel) -> None:
+        self.tokenizer          : transformers.PreTrainedTokenizer  = tokenizer
+        self.model              : transformers.PreTrainedModel      = model
+        self.model_saved_path   : Optional[str]                     = None
+
+    def train(self, train_texts: pd.Series, train_labels: pd.Series, training_args: transformers.TrainingArguments) -> transformers.Trainer:
+
+        train_texts_lst     : List[str] = train_texts.to_list()
+        train_labels_lst    : List[int] = train_labels.replace({ False: 0, True: 1 }, inplace=False).to_list()
+
+        train_encodings : transformers.BatchEncoding    = self.tokenizer(text=train_texts_lst, max_length=SEQUENCE_MAX_LENGTH,
+            truncation=SEQUENCE_TRUNCATION, padding=SEQUENCE_PADDING, return_tensors='pt')
+
+        train_dataset   : Dataset               = Dataset(train_encodings, train_labels_lst)
+        trainer         : transformers.Trainer  = transformers.Trainer(model=self.model, args=training_args, train_dataset=train_dataset)
+
+        trainer.train()
+        self.model_saved_path = os.path.join(training_args.output_dir, FINAL_MODEL_SAVE)
+        trainer.save_model(self.model_saved_path)
+
+    def __del__(self):
+        del self.model
+        del self.tokenizer
 
 # ======================================================================== MAIN EXECUTION ========================================================================
 
@@ -66,3 +143,14 @@ for path_to_extracted in arguments.files:
 final_dataframe : pd.DataFrame = pd.concat(dataframes)
 # Preprocess text
 final_dataframe[COLUMN_TEXT] = final_dataframe[COLUMN_TEXT].progress_apply(lambda text: preprocess_text(text))
+
+# Get basic structures for model development
+training_args           = get_training_args(output_dir, logging_dir)
+tokenizer, model_base   = get_xlm_roberta_large()
+# Get model
+model : TransformerModel = TransformerModel(tokenizer, model_base)
+# Develop model
+old_stdout = sys.stdout
+sys.stdout = open(os.devnull, "w")
+model.train(final_dataframe[COLUMN_TEXT], final_dataframe[COLUMN_VALENCE], training_args)
+sys.stdout = old_stdout
