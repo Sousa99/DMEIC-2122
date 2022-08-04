3d9223dbee8d3363e5a26e36e185db1d9ddc1e4b
Sousa99 <rod_sousa@hotmail.com>
2022-08-04 19:29:20 +0100

    Model execution back to current development ➕

--- models/model_second.py
+++ models/model_second.py
@@ -25,14 +25,14 @@ if print_variations or parallelization is None or parallelization == module_mode
 
     # =========================================== Get Features for FeatureSet ===========================================
 
-    #structure_feature_set = module_structure_features.StructureFeatureSet()
+    structure_feature_set = module_structure_features.StructureFeatureSet()
     content_feature_set = module_content_features.ContentFeatureSet()
     #entirety_feature_set = module_entirety_features.EntiretyFeatureSet()
-    #all_feature_set = module_featureset.MergedFeatureSetAbstraction([structure_feature_set, content_feature_set, entirety_feature_set])
+    all_feature_set = module_featureset.MergedFeatureSetAbstraction([structure_feature_set, content_feature_set])
 
     # Feature Sets
     #feature_sets : List[module_featureset.FeatureSetAbstraction] = [structure_feature_set, content_feature_set, entirety_feature_set, all_feature_set]
-    feature_sets : List[module_featureset.FeatureSetAbstraction] = [content_feature_set]
+    feature_sets : List[module_featureset.FeatureSetAbstraction] = [structure_feature_set, content_feature_set, all_feature_set]
 
     # =========================================== Init Execution of Feature Sets ===========================================
 
@@ -40,10 +40,10 @@ if print_variations or parallelization is None or parallelization == module_mode
 
         model.init_execution()
 
-        #structure_feature_set.init_execution(model.subjects_paths, model.PREFERENCE_AUDIO_TRACKS,
-        #    model.PREFERENCE_TRANS, model.EXTENSION_TRANS, model.subjects_infos, model.GENERAL_DROP_COLUMNS)
-        #structure_feature_set.develop_static_df()
-        #print(" -------------------- ")
+        structure_feature_set.init_execution(model.subjects_paths, model.PREFERENCE_AUDIO_TRACKS,
+            model.PREFERENCE_TRANS, model.EXTENSION_TRANS, model.subjects_infos, model.GENERAL_DROP_COLUMNS)
+        structure_feature_set.develop_static_df()
+        print(" -------------------- ")
 
         content_feature_set.init_execution(model.subjects_paths, model.PREFERENCE_AUDIO_TRACKS,
             model.PREFERENCE_TRANS, model.EXTENSION_TRANS, model.subjects_infos, model.GENERAL_DROP_COLUMNS)
@@ -55,8 +55,8 @@ if print_variations or parallelization is None or parallelization == module_mode
         #entirety_feature_set.develop_static_df()
         #print(" -------------------- ")
 
-        #all_feature_set.init_execution(model.subjects_paths, model.PREFERENCE_AUDIO_TRACKS,
-        #    model.PREFERENCE_TRANS, model.EXTENSION_TRANS, model.subjects_infos, model.GENERAL_DROP_COLUMNS)
+        all_feature_set.init_execution(model.subjects_paths, model.PREFERENCE_AUDIO_TRACKS,
+            model.PREFERENCE_TRANS, model.EXTENSION_TRANS, model.subjects_infos, model.GENERAL_DROP_COLUMNS)
 
         for feature_set in feature_sets: feature_set.develop_static_df()
 
